6abb77dba29ecc4fca7a18c0c4e73e9cc7fb990d
jason1642 <jason1642@gmail.com>
2026-09-19 17:17:15 -0400

    product url serializers

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.filter(is_active=True).select_related("category")
+    serializer_class = ProductSerializer
